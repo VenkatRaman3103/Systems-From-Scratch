83ddc2092882aba5ef8bf3af256c41bfea7e1869
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2026-03-03 22:00:57 +0530

    feat(React): test on useState

--- React/src/main.js
+++ React/src/main.js
@@ -2,18 +2,19 @@ import { createElement, render, useState } from "./engine.js";
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState(true);
 
   return createElement(
     "div",
     null,
     createElement("h1", null, count),
+    createElement("p", null, text ? "hello" : "world"),
     createElement(
       "button",
-      {
-        onclick: () => setCount((c) => c + 1),
-      },
+      { onclick: () => setCount((c) => c + 1) },
       "Increment",
     ),
+    createElement("button", { onclick: () => setText(!text) }, "Change Text"),
   );
 }
 
